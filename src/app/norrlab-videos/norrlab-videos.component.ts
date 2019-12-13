21f07be42cafe2e7f6743780c11238de7be3049f
Ulrich Ossene <ulrich.ossene@gmail.com>
2019-12-13 22:11:50 +0100

    [0005] vase -- trades page&video page

--- src/app/norrlab-videos/norrlab-videos.component.ts
+++ src/app/norrlab-videos/norrlab-videos.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+
 
 @Component({
   selector: 'app-norrlab-videos',
   templateUrl: './norrlab-videos.component.html',
-  styleUrls: ['./norrlab-videos.component.scss']
+  styleUrls: ['./norrlab-videos.component.css']
 })
 export class NorrlabVideosComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
-
+  } 
 }
