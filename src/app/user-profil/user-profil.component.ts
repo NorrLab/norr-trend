906fdc2c8936487973d8e85c84e50aceba7b62d4
Ulrich Ossene <ulrich.ossene@gmail.com>
2020-03-18 22:38:47 +0100

    [0098] vase -- user profil :  -- page  --ts,css,html

--- src/app/user-profil/user-profil.component.ts
+++ src/app/user-profil/user-profil.component.ts
@@ -7,6 +7,7 @@ import { DOCUMENT } from '@angular/platform-browser';
 import { Inject,Injectable } from '@angular/core';
 import {COMMA, ENTER} from '@angular/cdk/keycodes'; 
 import {MatChipInputEvent} from '@angular/material';
+import {environment} from './../../environments/environment.prod'; 
 
 
 @Component({
@@ -17,11 +18,23 @@ import {MatChipInputEvent} from '@angular/material';
 export class UserProfilComponent implements OnInit {
    _norrUser;
    __norrSubscribers = [1,2,3,4,5,6,7,8];
+   pictureUrl;
   constructor(private userService:UserService,private toastrService:ToastrService,private activatedRoute: ActivatedRoute) { }
+
+  getUserpicture(){
+    if(this._norrUser){
+
+    console.log(`${environment.apiUrl+this._norrUser.pictureUrl} `)
+    return environment.apiUrl+this._norrUser.pictureUrl
+    }
+    return '';
+  }
+
   ngOnInit() {
   	this.userService.getUserById(this.activatedRoute.snapshot.params.userId)
   	.subscribe(user =>{
   		this._norrUser = user; 
+      this.pictureUrl = environment.apiUrl+'/images'+user.userPictureUrl;
   	},err =>{
   		//TODO
   		window.history.back()
@@ -29,3 +42,6 @@ export class UserProfilComponent implements OnInit {
   }
 
 }
+
+
+// http://localhost:369/{{images/community-user/ossene-profile.jpg}}
